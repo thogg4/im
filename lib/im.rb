0aaed37ff8ba6401701f6bb9ef2e419c90aed893
Tim <thogg4@gmail.com>
2014-02-13 10:43:54 -0700

    fix bug

--- lib/im.rb
+++ lib/im.rb
@@ -5,7 +5,8 @@ module Im
   class << self
 
 	def send_message(message, chat)
-      puts "Sending \'#{message}\' to #{chat[:name]}"
+      puts chat
+      puts "Sending '#{message}' to #{chat[:name]}"
       `osascript -e 'tell application "Messages" to send \"#{message}\" to item #{chat[:id].to_i} of text chats'`
 	end
 
@@ -13,12 +14,12 @@ module Im
       im = Appscript.app("Messages")
       participants = im.chats.participants.get()
       participants.map.with_index do |participant, index|
-        { id: index+1, name: participant.first.name.get().lstrip, object: participant.first }
+        { id: index+1, name: participant.first.name.get(), object: participant.first }
       end
 	end
 
     def get_chat_from_number(chats, number)
-      chats.select { |chat| chat[:id] == number }
+      chats.select { |chat| chat[:id] == number.to_i }[0]
     end
 
   end
